8ff6d278c57d03bb9956a96a548980e362cb800e
Habtamu Getahun <hassefa8@gmail.com>
2023-09-26 17:08:51 +0300

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,56 +1,43 @@
 #include "lists.h"
 
 /**
- *
  * insert_nodeint_at_index - Inserts node at index of list.
  * @head: Double pointer to head of list
  * @idx: Index to insert node
  * @n: Data for new node
  * Return: New node or NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *current;
-	listint_t *previous;
 	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	if (head == NULL)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	if (idx == 0 && *head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	current = *head;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
-		new_node->next = current;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
-	for (i = 0; i < idx - 1 && current != NULL; i++)
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (i == idx - 2)
-			previous = current;
-
-		Copy
-			current = current->next;
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
-
-	if (current == NULL)
-		return (NULL);
-	previous->next = new_node;
-	new_node->next = current;
-	return (new_node);
+	return (NULL);
 }
