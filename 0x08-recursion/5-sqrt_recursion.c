ce5f3bbdc0a90d31f8b1d3b2f3a3839a35e738c3
Habtamu Getahun <hassefa8@gmail.com>
2023-11-15 11:24:24 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,42 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: value to raise
- * @y: power
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to calculate the square root of.
  *
- * Return: result of the power
+ * Return: The natural square root of the number, or -1 if it does not exist.
  */
-int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	if (n < 0)
 		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (sqrt_recursive(n, 1, n));
+}
+
+/**
+ * sqrt_recursive - Helper function for recursive calculation of square root.
+ * @n: The number to calculate the square root of.
+ * @start: The starting point for the search.
+ * @end: The ending point for the search.
+ *
+ * Return: The natural square root of the number, or -1 if it does not exist.
+ */
+int sqrt_recursive(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+
+	if (start <= end)
+	{
+		if (mid * mid == n)
+			return (mid);
+		if (mid * mid < n)
+			return (sqrt_recursive(n, mid + 1, end));
+
+		return (sqrt_recursive(n, start, mid - 1));
+	}
+	return (end);
 }
